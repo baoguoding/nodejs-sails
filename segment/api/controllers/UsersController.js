a0f8fee575e15c74e494b60ae79bd34893dd859b
baoguoding <baoguoding@126.com>
2016-11-14 16:07:26 +0800

    update

---
+++ segment/api/controllers/UsersController.js
@@ -0,0 +1,31 @@
+/**
+ * UsersController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `UsersController.zhuce()`
+   */
+  zhuce: function (req, res) {
+    return res.json({
+      todo: 'zhuce() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UsersController.login()`
+   */
+  login: function (req, res) {
+    return res.json({
+      todo: 'login() is not implemented yet!'
+    });
+  }
+};
+
