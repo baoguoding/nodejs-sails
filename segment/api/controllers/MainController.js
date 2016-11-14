bb12fbd4ce9b7b92934e04626157edc3fc9ac487
baoguoding <baoguoding@126.com>
2016-11-14 22:24:28 +0800

    update

---
+++ segment/api/controllers/MainController.js
@@ -0,0 +1,28 @@
+/**
+ * MainController
+ *
+ * @description :: Server-side logic for managing mains
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+
+
+module.exports = {
+
+
+
+    /**
+     * `MainController.index()`
+     */
+    index: function (req, res) {
+        loginbean = req.session.loginbean;
+        console.log(loginbean);
+        res.view('index',{loginbean:loginbean});
+    },
+    logout:function(req,res){
+        req.session.destroy(function(err) {
+            res.redirect('/');
+        })
+    }
+};
+
